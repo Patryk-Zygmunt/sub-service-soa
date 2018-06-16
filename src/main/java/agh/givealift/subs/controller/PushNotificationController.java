fc3952735ecc135229c2327e577b39318fdfc702
Bartek Grodek <bartgrdk@gmail.com>
2018-06-17 00:24:03 +0200

    enable CORS

--- src/main/java/agh/givealift/subs/controller/PushNotificationController.java
+++ src/main/java/agh/givealift/subs/controller/PushNotificationController.java
@@ -9,11 +9,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.util.UriComponentsBuilder;
 
+@CrossOrigin
 @Controller
 @RequestMapping("/api/notification")
 public class PushNotificationController {
